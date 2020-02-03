772cde74af0317a776e7789da83020925d16dd52
Inna Udemy <inna@gmail.com>
2020-02-04 00:57:56 +0100

    second commit in Test

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main (String[] args){
 
         System.out.println("FirstTest");
-        System.out.println("bla bla bla");
+        System.out.println("bla bla bla......");
 
 
     }
